d092d00836a1b41fa8dc56867268f4a42b8674d0
saw-cco <112614204+saw-cco@users.noreply.github.com>
2023-01-28 10:06:45 +0100

    changed AppComponent to standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [RouterModule]
 })
 export class AppComponent {
   title = 'angular-play';
